214ee29fe4f923f670efd57d08a70df54f767117
Aodxfaionn <aodxfaionn@yandex.ru>
2024-05-26 02:54:57 +0300

    page 1920

--- js/script.js
+++ js/script.js
@@ -13,6 +13,35 @@ const swiper = new Swiper(".swiper", {
   },
 });
 
+const swiperProduction = new Swiper(".productionSlider", {
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  allowTouchMove: true,
+  breakpoints: {
+    768: {
+      allowTouchMove: false,
+    },
+  },
+});
+
+const swiperPartners = new Swiper(".partnersSlider", {
+  loop: true,
+  allowTouchMove: true,
+  slidesPerView: "auto"
+});
+
+const swiperWorks = new Swiper(".worksSlider", {
+  loop: true,
+  allowTouchMove: true,
+  slidesPerView: "auto",
+  spaceBetween: 40,
+});
+
+
+
 // Бургерное меню
 // const burger = document.querySelector(".burger"),
 //   contacts = document.querySelector(".header__contacts"),
